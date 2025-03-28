1cb7f9a101d534876c0d4a7142099d7ee0174bf9
AbolfazlJahangir <jahangirabolfazl@gmail.com>
2025-03-28 05:08:31 +0330

    Database reader class

--- files/database_read.py
+++ files/database_read.py
@@ -10,29 +10,37 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-def get_data(name):
-    try:
-        conn = mysql.connector.connect(
-            host=DB_HOST,
-            user=DB_USER,
-            password=DB_PASSWORD,
-            database=DB_NAME)
-        cursor = conn.cursor()
+class DatabaseReader:
+    def __init__(self, name=None):
+        self.name = name
 
+    def set_name(self, name):
+        self.name = name
+
+    def get_data(self, name):
         try:
-            cursor.execute("SELECT embedding FROM users WHERE name = %s;", (name, ))
-            res = cursor.fetchone()
-            embedding = np.frombuffer(res[0], dtype=np.float64)
-            return embedding
+            self.set_name(name)
+            conn = mysql.connector.connect(
+                host=self.DB_HOST,
+                user=self.DB_USER,
+                password=self.DB_PASSWORD,
+                database=self.DB_NAME)
+            cursor = conn.cursor()
 
-        except Exception as execution_error:
-            print(f"There is a problem in query execution:\n{execution_error}")
+            try:
+                cursor.execute("SELECT embedding FROM users WHERE name = %s;", (name, ))
+                res = cursor.fetchone()
+                embedding = np.frombuffer(res[0], dtype=np.float64)
+                return embedding
 
-    except mysql.connector.Error as connection_error:
-        print(f"There is a problem in connection:\n{connection_error}")
+            except Exception as execution_error:
+                print(f"There is a problem in query execution:\n{execution_error}")
 
-    finally:
-        if 'conn' in locals() and conn.is_connected():
-            conn.close()
-            cursor.close()
-            print("Database connection closed.")
+        except mysql.connector.Error as connection_error:
+            print(f"There is a problem in connection:\n{connection_error}")
+
+        finally:
+            if 'conn' in locals() and conn.is_connected():
+                conn.close()
+                cursor.close()
+                print("Database connection closed.")
